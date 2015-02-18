219c1bac7f275ed2de76c05a1f6e81990759d638
David Irvine <david.irvine@maidsafe.net>
2015-02-18 21:29:44 +0000

    fix for win32 bit and win 64 bit builds

--- cmake/Toolchain-mingw64.cmake
+++ cmake/Toolchain-mingw64.cmake
@@ -8,15 +8,13 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
-set(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
-
-
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 # here is the target environment located
 set(CMAKE_FIND_ROOT_PATH  /usr/i686-mingw32 )
 
-include_directories(/usr/i686-w64-mingw32/include/)
+include_directories(/usr/lib/x86_64-w64-mingw32/include/)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
