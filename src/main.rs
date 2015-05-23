43fe16a882d9089fa51450ab43a6c369d581dfb5
David Irvine <david.irvine@maidsafe.net>
2015-05-24 00:11:27 +0100

    docs etc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#![doc(html_logo_url = "http://maidsafe.net/img/Resources/branding/maidsafe_logo.fab2.png",
+       html_favicon_url = "http://maidsafe.net/img/favicon.ico",
+              html_root_url = "http://dirvine.github.io/wwww")]
+ #![warn(missing_docs)]
+#![allow(dead_code, unused_variables, unused_features, unused_attributes)]
+#![feature(io, core)]
+
 fn main() {
     println!("Hello, world!");
 }
