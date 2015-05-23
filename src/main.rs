7b0bced31b4d53ad6aa22758dc221ac8fba19fd1
David Irvine <david.irvine@maidsafe.net>
2015-05-24 00:34:31 +0100

    fix warnings

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 #![doc(html_logo_url = "http://maidsafe.net/img/Resources/branding/maidsafe_logo.fab2.png",
        html_favicon_url = "http://maidsafe.net/img/favicon.ico",
               html_root_url = "http://dirvine.github.io/wwww")]
- #![warn(missing_docs)]
-#![allow(dead_code, unused_variables, unused_features, unused_attributes)]
-#![feature(io, core)]
+//! wwww
+#![deny(missing_docs, dead_code, unused_variables, unused_features, unused_attributes)]
+#[cfg_attr(test, allow(dead_code))]
+// #![feature(io, core)]
 
 fn main() {
     println!("Hello, world!");
+}
+
+
+#[cfg(test)]
+mod test {
+#[test]     
+fn it_works() {}    
+    
 }
